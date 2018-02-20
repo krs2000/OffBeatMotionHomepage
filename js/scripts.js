163ce180b4c40700a43b4631a3a2f78793a1bae4
krs2000 <krskrysiak@gmail.com>
2018-02-20 15:17:53 +0100

    cds

--- js/scripts.js
+++ js/scripts.js
@@ -1,102 +1,105 @@
-var navSize=90;
+var navSize = 90;
 
 
-var vimeoEmebed=["249434988","248575397","233169986","233168772","233168159","233167640","233166754","233165733","233164515"];
+var vimeoEmebed = ["249434988", "248575397", "233169986", "233168772", "233168159", "233167640", "233166754", "233165733", "233164515"];
 
 // var hotel ='233170709';
 
-function vimeoMovie(embed){
-	return "<div class=\"vimeoMovieContainer fadeIn\"><iframe class=\"vimeoMovie\" src=\"https://player.vimeo.com/video/"+embed+"?title=0&byline=0&portrait=0\"  frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>"
+function vimeoMovie(embed) {
+    return "<div class=\"vimeoMovieContainer fadeIn\"><iframe class=\"vimeoMovie\" src=\"https://player.vimeo.com/video/" + embed + "?title=0&byline=0&portrait=0\"  frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>"
 }
 
-function videoImg(src){
-	return "<div class=\"miniVideoConatainer\" data-id="+src+"> <img class=\"miniVideo\"  src=\"./img/mini/"+src+".jpg\" alt=\"sample video\"/><div class=\"mask\"><img class=\"playButton\" src=\"./img/play.png\" alt=\"play\"></div></div>"
+function videoImg(src) {
+    return "<div class=\"miniVideoConatainer\" data-id=" + src + "> <img class=\"miniVideo\"  src=\"./img/mini/" + src + ".jpg\" alt=\"sample video\"/><div class=\"mask\"><img class=\"playButton\" src=\"./img/play.png\" alt=\"play\"></div></div>"
 }
 
 document.onload = loadVideos();
-$('.mainVideo').onload= function(){$('.mainVideo').play()}
-
-
-function loadVideos(){
-for(var i = 1 ; i< 10; i++) {
-  var video = videoImg(i);
-document.getElementById("content").innerHTML +=  video;
-}
+var mainVideo = $('.mainVideo');
+mainVideo.onload = function(){
+ mainVideo.play();
 }
 
 
+function loadVideos() {
+    for (var i = 1; i < 10; i++) {
+        var video = videoImg(i);
+        document.getElementById("content").innerHTML += video;
+    }
+}
 
 
-$('.videosLink').click(function(){
- positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
- $("html, body").animate({scrollTop:positionabout}, '500', 'swing');
-  $('#content').removeClass();
-  $('#content').html("");
- $('#content').addClass("videoList fadeIn");
- loadVideos();
+
+
+$('.videosLink').click(function() {
+    positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
+    $("html, body").animate({ scrollTop: positionabout }, '500', 'swing');
+    $('#content').removeClass();
+    $('#content').html("");
+    $('#content').addClass("videoList fadeIn");
+    loadVideos();
 })
 
-$('.scrollDown').click(function(){
- positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
- $("html, body").animate({scrollTop:positionabout}, '500', 'swing');
+$('.scrollDown').click(function() {
+    positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
+    $("html, body").animate({ scrollTop: positionabout }, '500', 'swing');
 })
 
-$('.aboutLink').click(function(){
- positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
- $("html, body").animate({scrollTop:positionabout}, '500', 'swing');
-  $('#cover').html(returnMainVideo());
- $('#content').removeClass()
-$('#content').html("");
- $('#content').addClass("about");
-$('#content').html(returnTimeline());
+$('.aboutLink').click(function() {
+    positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
+    $("html, body").animate({ scrollTop: positionabout }, '500', 'swing');
+    $('#cover').html(returnMainVideo());
+    $('#content').removeClass()
+    $('#content').html("");
+    $('#content').addClass("about");
+    $('#content').html(returnTimeline());
 })
 
-$('.clientsLink').click(function(){
- positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
- $("html, body").animate({scrollTop:positionabout}, '500', 'swing');
-  $('#cover').html(returnMainVideo());
- $('#content').removeClass();
- $('#content').html("");
- $('#content').addClass("clients");
-$('#content').html(returnClients());
+$('.clientsLink').click(function() {
+    positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
+    $("html, body").animate({ scrollTop: positionabout }, '500', 'swing');
+    $('#cover').html(returnMainVideo());
+    $('#content').removeClass();
+    $('#content').html("");
+    $('#content').addClass("clients");
+    $('#content').html(returnClients());
 })
 
-$('.contactLink').click(function(){
- positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
- $("html, body").animate({scrollTop:positionabout}, '500', 'swing');
-  $('#cover').html(returnMainVideo());
- $('#content').removeClass();
- $('#content').html("");
- $('#content').addClass("contact fadeIn");
-$('#content').html(returnContact());
-})
-
-
-
-$('.logo').click(function(){
- $('#cover').html(returnMainVideo());
-
- positionabout = $('#cover').offset().top - navSize; // Position of #about - nav height = correct position
- $("html, body").animate({scrollTop:positionabout}, '500', 'swing');
+$('.contactLink').click(function() {
+    positionabout = $('#content').offset().top - navSize; // Position of #about - nav height = correct position
+    $("html, body").animate({ scrollTop: positionabout }, '500', 'swing');
+    $('#cover').html(returnMainVideo());
+    $('#content').removeClass();
+    $('#content').html("");
+    $('#content').addClass("contact fadeIn");
+    $('#content').html(returnContact());
 })
 
 
 
-$(document).on('click', '.miniVideoConatainer', function(){
- positionabout = $('#cover').offset().top - navSize; // Position of #about - nav height = correct position
- $("html, body").animate({scrollTop:positionabout}, '500', 'swing');
-  // $('#cover').removeClass();
+$('.logo').click(function() {
+    $('#cover').html(returnMainVideo());
+
+    positionabout = $('#cover').offset().top - navSize; // Position of #about - nav height = correct position
+    $("html, body").animate({ scrollTop: positionabout }, '500', 'swing');
+})
+
+
+
+$(document).on('click', '.miniVideoConatainer', function() {
+    positionabout = $('#cover').offset().top - navSize; // Position of #about - nav height = correct position
+    $("html, body").animate({ scrollTop: positionabout }, '500', 'swing');
+    // $('#cover').removeClass();
     $('#cover').html("");
-  $('#cover').html(vimeoMovie(vimeoEmebed[$(this).attr("data-id")]));
- // $('#content').addClass("videoList");
+    $('#cover').html(vimeoMovie(vimeoEmebed[$(this).attr("data-id")]));
+    // $('#content').addClass("videoList");
 
 })
 
 
 
-$(window).load(function(){
+$(window).load(function() {
     $('#spinner').fadeOut(1000);
-   $('html, body').css('overflowY', 'auto'); 
+    $('html, body').css('overflowY', 'auto');
 })
 
 
@@ -115,84 +118,84 @@ $(window).load(function(){
 
 
 
-function returnClients(){
-return(
+function returnClients() {
+    return (
 
 
-        "<div class=\"clientsContainer fadeIn\">"+
-            "<img class=\"clientImg\" src=\"./img/Clients/1.png\" alt=\"\">"+
-            "<img class=\"clientImg\" src=\"./img/Clients/2.png\" alt=\"\">"+
-            "<img class=\"clientImg\" src=\"./img/Clients/3.png\" alt=\"\">"+
-            "<img class=\"clientImg\" src=\"./img/Clients/4.png\" alt=\"\">"+
-            "<img class=\"clientImg\" src=\"./img/Clients/5.png\" alt=\"\">"+
-        "</div>"+
-    "</section>"
-   
+        "<div class=\"clientsContainer fadeIn\">" +
+        "<img class=\"clientImg\" src=\"./img/Clients/1.png\" alt=\"\">" +
+        "<img class=\"clientImg\" src=\"./img/Clients/2.png\" alt=\"\">" +
+        "<img class=\"clientImg\" src=\"./img/Clients/3.png\" alt=\"\">" +
+        "<img class=\"clientImg\" src=\"./img/Clients/4.png\" alt=\"\">" +
+        "<img class=\"clientImg\" src=\"./img/Clients/5.png\" alt=\"\">" +
+        "</div>" +
+        "</section>"
 
 
 
-	)
+
+    )
 }
 
-function returnContact(){
-return(
-"<h2>Get in toutch with us .</h2>"+
-      "<div class=\"emailForm\">"+
-           " <form class=\"formData\" action=\"https://formspree.io/krskrysiak@gmail.com\" method=\"POST\">"+
-                "<input type=\"text\" name=\"name\" placeholder=\"Name\">"+
-                "<input type=\"text\" name=\"name\" placeholder=\"Topic\">"+
-               " <input type=\"text\" class=\"textData\" name=\"message\" placeholder=\"Your message\">"+
-               " <input type=\"email\" class=\"emailData\" name=\"_replyto\" placeholder=\"Email\">"+
-              "  <input type=\"submit\" value=\"Send\">"+
-           " </form>"+
-       " </div>  "
-)
-    }
+function returnContact() {
+    return (
+        "<h2>Get in toutch with us .</h2>" +
+        "<div class=\"emailForm\">" +
+        " <form class=\"formData\" action=\"https://formspree.io/krskrysiak@gmail.com\" method=\"POST\">" +
+        "<input type=\"text\" name=\"name\" placeholder=\"Name\">" +
+        "<input type=\"text\" name=\"name\" placeholder=\"Topic\">" +
+        " <input type=\"text\" class=\"textData\" name=\"message\" placeholder=\"Your message\">" +
+        " <input type=\"email\" class=\"emailData\" name=\"_replyto\" placeholder=\"Email\">" +
+        "  <input type=\"submit\" value=\"Send\">" +
+        " </form>" +
+        " </div>  "
+    )
+}
 
 
 
- function returnTimeline(){
- 	return(
-  "<div class=\"timeline fadeIn\">"+
-    "<ul>"+
-     " <li>"+
-        "<h3>Make Script</h3>"+
-       " <p>One day, my boss came up to me and asked, Pretty much decided I wasn't ready.</p>"+
-      "</li>"+
-      "<li>"+
-        "<h3>Film</h3>"+
-       " <p>Began work at the Button Factory. This was probably the best time of my life. I had a wife, a dog, and a family.</p>"+
-      "</li>"+
-      "<li>"+
-        "<h3>Editing</h3>"+
-        "<p>I was just sick of Josh</p>"+
-      "</li>"+
-      "<li>"+
-       " <h3>Consulting Result</h3>"+
-       " <p>I moved to and settled down into my new life.</p>"+
-     " </li>"+
-    "</ul>"+
-    "<ul>"+
-      "<li>"+
-        "<h3>Deliver</h3>"+
-        "<p>It only took, like, two years.</p>"+
-      "</li>"+
-   " </ul>"+
-  "</div>"
- 		
- 		)
- }
+function returnTimeline() {
+    return (
+        "<div class=\"timeline fadeIn\">" +
+        "<ul>" +
+        " <li>" +
+        "<h3>Make Script</h3>" +
+        " <p>One day, my boss came up to me and asked, Pretty much decided I wasn't ready.</p>" +
+        "</li>" +
+        "<li>" +
+        "<h3>Film</h3>" +
+        " <p>Began work at the Button Factory. This was probably the best time of my life. I had a wife, a dog, and a family.</p>" +
+        "</li>" +
+        "<li>" +
+        "<h3>Editing</h3>" +
+        "<p>I was just sick of Josh</p>" +
+        "</li>" +
+        "<li>" +
+        " <h3>Consulting Result</h3>" +
+        " <p>I moved to and settled down into my new life.</p>" +
+        " </li>" +
+        "</ul>" +
+        "<ul>" +
+        "<li>" +
+        "<h3>Deliver</h3>" +
+        "<p>It only took, like, two years.</p>" +
+        "</li>" +
+        " </ul>" +
+        "</div>"
+
+    )
+}
 
 
- function returnMainVideo(){
-return(
-  "<div class=\"mainVideoCotainer fadeIn\">"+
-    "<video autoplay loop class=\"mainVideo\">"+
-    "<source src=\"./img/cover.mp4\" type=\"video/mp4\">"+
-    "</video>"+
-    "</div>"+
-           "<img  src=\"./img/logo.png\" alt=\"logo\">"+
+function returnMainVideo() {
+    return (
+        "<div class=\"mainVideoCotainer fadeIn\">" +
+        "<video autoplay loop class=\"mainVideo\">" +
+        "<source src=\"./img/cover.mp4\" type=\"video/mp4\">" +
+        "</video>" +
+        "</div>" +
+        "<img  src=\"./img/logo.png\" alt=\"logo\">" +
         "<a class=\"scrollDown pulse\" href=\"#content\"></a>"
-  
-  )
- }
+
+    )
+}
